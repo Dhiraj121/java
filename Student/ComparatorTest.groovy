04bad34fac8ca8c1358093bb1a03eb72adfd5d8c
unknown <dhirajgadekar1997@gmail.com>
2018-07-17 19:47:48 -0700

    first java

---
+++ Student/ComparatorTest.groovy
@@ -0,0 +1,7 @@
+package Student
+
+/**
+ * Created by D & G on 3/31/2018.
+ */
+class ComparatorTest extends groovy.util.GroovyTestCase {
+}
---
+++ Student/HumanTest.groovy
@@ -0,0 +1,7 @@
+package Student
+
+/**
+ * Created by D & G on 3/23/2018.
+ */
+class HumanTest extends groovy.util.GroovyTestCase {
+}
